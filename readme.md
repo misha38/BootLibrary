1a3b6d29777b442def0beea8a2927c7890be438b
mtarasenko <mtarasenko@aquivalabs.com>
2022-09-21 20:58:11 +0300

    Readme added

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Library with Spring Boot
+
+Note:
+'/getBooks' and '/getBooksByLetter' requests are implemented via single URL: 
+    
+    - '/lib/books' returns all the books' JSONs;
+    - '/lib/books?letter=x' returns only the books which name starts with the letter parameter value;
+
+Sample DB with 2 records is deployed automatically; 
+
+
--- src/main/java/cj/Sber/BootLibrary/BootLibraryApplication.java
+++ src/main/java/cj/Sber/BootLibrary/BootLibraryApplication.java
@@ -1,20 +1,12 @@
 package cj.Sber.BootLibrary;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BootLibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootLibraryApplication.class, args);
-
-
-	}
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
 	}
 }
